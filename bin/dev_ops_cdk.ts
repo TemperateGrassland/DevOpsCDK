a41bb816b6a141cd955fcef2d9177aa6c82c147e
Charlie Allen <callnan@amazon.com>
2022-09-13 13:24:59 +0100

    Updated region

--- bin/dev_ops_cdk.ts
+++ bin/dev_ops_cdk.ts
@@ -5,7 +5,7 @@ import { DevOpsCdkPipelineStack } from '../lib/dev_ops_cdk_pipeline-stack';
 const app = new App();
 
 new DevOpsCdkPipelineStack(app, 'DevOpsCdkPipelineStack', {
-    env: { account: 'charlie_apprentice', region: 'eu-west-2' },
+    env: { account: 'charlie_apprentice', region: 'eu-west-1' },
 });
 
 app.synth();
